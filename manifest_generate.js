5d249ccbe4a428b8af5932a65c5085607a3f8132
john li <jetpack@google.com>
2019-02-15 15:41:03 -0500

    version bump

--- manifest_generate.js
+++ manifest_generate.js
@@ -70,8 +70,8 @@ const DEV_CONFIG = {
 
 const PROD_CONFIG = {
   'name': 'Tune (beta)',
-  'version': '0.2.1',
-  'version_name': '0.2.1 beta-testing',
+  'version': '0.2.2',
+  'version_name': '0.2.2 beta-testing',
   'content_security_policy': "script-src 'self' https://www.google.com https://www.google-analytics.com https://support.google.com https://www.gstatic.com; object-src 'self'",
 };
 
